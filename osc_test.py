cd5d8d705c30d554e91401d6eaf75ecfd6e3b996
walkerkaiman <walkerkaiman@gmail.com>
2025-07-04 19:49:00 -0500

    Update osc_test.py

--- osc_test.py
+++ osc_test.py
@@ -2,7 +2,7 @@ from pythonosc.udp_client import SimpleUDPClient
 import time
 
 # Set these values to match your OSC Audio Player
-TARGET_IP = "192.168.4.21"  # Use your computer's IP or 127.0.0.1 if local
+TARGET_IP = "127.0.0.1"  # Use your computer's IP or 127.0.0.1 if local
 TARGET_PORT = 8000       # Match the OSC port in the GUI
 OSC_ADDRESS = "/ping"    # Match the OSC address entered in the GUI
 
